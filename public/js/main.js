4ab8e7b11ae52ffe4b62ed7731a1fd42d2fe69e7
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 00:38:49 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+// ex1
+// let number1 = 5;
+// let number2 = 10;
+// console.log(number1 + number2);
+// console.log(number1 - number2);
+// console.log(number1 / number2);
+// console.log(number1 * number2);
+
+// ex2
+
+let prenom = prompt('prenom?');
+let age = parseInt(prompt('age?'));
+// console.log(`${prenom} a ${age} ans cette année`);
+console.log(typeof age);
+console.log(`${prenom} aura ${age + 3} ans dans 3 ans`);
